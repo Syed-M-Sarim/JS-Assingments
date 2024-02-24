a3345ebd80310610f849070f1a2b8d67a12da1bd
Syed Muhammad Sarim <iamsarim1000@gmail.com>
2024-02-24 07:36:51 +0500

    Assingment-1 completed

--- Assingments/Assingment-1/app.js
+++ Assingments/Assingment-1/app.js
@@ -2,4 +2,15 @@
 var greetings = "Hello"
 
 alert(greetings)
+//q2
+var bug = "Sorry, Please enter a valid password"
+
+alert(bug)
+//q3
+var intro = "Welcome to JS Land... \n Happy Coding"
+
+alert(intro)
+//q4 done
+
+
 
