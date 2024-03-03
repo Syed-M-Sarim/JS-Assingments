b425faa227e29806c42b23e33b23dd0d0bb4a63b
Syed Muhammad Sarim <iamsarim1000@gmail.com>
2024-03-03 16:09:19 +0500

    q3 done

--- Assingments/Assingment-5/app.js
+++ Assingments/Assingment-5/app.js
@@ -2,25 +2,55 @@
 
 var z = 10 + 5
 
-document.write("10 + 5 = " + z )
+document.write("10 + 5 = " + z)
+document.write("<br> <br>")
 
 //q2
 
 var num1 = 10
-
 var num2 = 20
-
 var num3 = 30
-
 var num4 = 40
 
-console.log(num4 - num1)
-
-console.log(num1 * num2)
-
-console.log(num3 / num1)
-
-console.log(num4 % num3)
+document.write(num4 - num1)
+document.write("<br> <br>")
+document.write(num1 * num2)
+document.write("<br> <br>")
+document.write(num3 / num1)
+document.write("<br> <br>")
+document.write(num4 % num3)
+document.write("<br> <br>")
 
 //q3
+//a
+
+var value = "??"
+
+//b
+
+document.write("Value after variable declaration is: " + value)
+document.write("<br> <br>")
+
+//c
+
+var order = 5
+
+//d
+
+document.write("Initial value : " + order)
+document.write("<br>")
+
+//e
+
+var neworder = ++order
+document.write("Value after increment is : " + order)
+document.write("<br>")
+order = order + 7
+document.write("Value after addition is: " + order)
+var oldorder = --order
+document.write("<br>")
+document.write("Value after decrement is : " + order)
+order = 0 / order 
+document.write("<br>")
+document.write("The remainder is : " + order)
 
