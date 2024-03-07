271f7c4ade33b21200eca3fe045bee749f1cef80
Syed Muhammad Sarim <iamsarim1000@gmail.com>
2024-03-07 20:52:34 +0500

    new start

--- Assingments/Chapter - 12,13/app.js
+++ Assingments/Chapter - 12,13/app.js
@@ -12,3 +12,30 @@ else{
 
 //q2
 
+var value1 = +prompt("Enter value: ")
+var value2 = +prompt("Enter value: ")
+
+if (value1 >= value2){
+    document.write("This value is greater: 90" + value1)
+    document.write("<br>")
+}
+else{
+    document.write("This value is greater: " + value2)
+    document.write("<br>")
+}
+
+//q3 
+
+var data = +prompt("Enter number: ")
+
+if (data <= 0 ){
+    document.write("Negative")
+    document.write("<br>")
+}
+else{
+    document.write("Positive")
+    document.write("<br>")
+}
+
+//q4
+
