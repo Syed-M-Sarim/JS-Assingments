ace0a313e717e78ffc8de5cab543d19498376fb7
Syed Muhammad Sarim <iamsarim1000@gmail.com>
2024-03-14 23:27:22 +0500

    Done

--- Assingments/Array methods/app.js
+++ Assingments/Array methods/app.js
@@ -1,12 +1,35 @@
 //task
-
 var fruits = []
 
-fruits.push = ('Kiwi')
-fruits.push = ('Apple')
-fruits.push = ('Banana')
-fruits.push = ('Cherry')
-fruits.push = ('Pomegranet')
+fruits.push("Apple", "Strawberry" , "Guawa" , "Watermelon", "Kiwi")
+document.write("Items: " + fruits)
+document.write("<br><br>")
+document.write(fruits.length)
+document.write("<br><br>")
+document.write(fruits[0])
+document.write("<br><br>")
+document.write(fruits[1])
+document.write("<br><br>")
+document.write(fruits[2])
+document.write("<br><br>")
+document.write(fruits[3])
+document.write("<br><br>")
+document.write(fruits[4])
+document.write("<br><br>")
 
-console.log('Lists of items: ', fruits)
-console.log(fruits.length)
+fruits.pop()
+document.write("Items: " + fruits)
+document.write("<br><br>")
+
+fruits.shift()
+document.write("Items: " + fruits)
+document.write("<br><br>")
+
+fruits.unshift("Pomegranet")
+document.write("Items: " + fruits)
+document.write("<br><br>")
+
+//slice
+
+var update =  fruits.slice(0, 1)
+document.write("Items: " + update)
